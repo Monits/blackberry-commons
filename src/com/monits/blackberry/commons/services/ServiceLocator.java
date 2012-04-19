d806f0aa2a2410873431815b9644b1cd07b8906a
Rodrigo Pereyra <rpereyra@monits.com>
2012-04-19 15:51:44 -0300

    Add ScreenTypeService to the ServiceLocator.

--- src/com/monits/blackberry/commons/services/ServiceLocator.java
+++ src/com/monits/blackberry/commons/services/ServiceLocator.java
@@ -4,6 +4,7 @@ import java.util.Hashtable;
 
 import com.monits.blackberry.commons.services.implementations.ImageResizeServiceImpl;
 import com.monits.blackberry.commons.services.implementations.ResourceServiceImpl;
+import com.monits.blackberry.commons.services.implementations.ScreenTypeServiceImpl;
 
 public class ServiceLocator {
 	private static Hashtable services;
@@ -12,13 +13,18 @@ public class ServiceLocator {
 		services = new Hashtable();
 		services.put(ImageResizeService.class, new ImageResizeServiceImpl());
 		services.put(ResourceService.class, new ResourceServiceImpl());
+		services.put(ScreenTypeService.class, new ScreenTypeServiceImpl());
 	}
-
+	
 	public static ImageResizeService getImageResizeService() {
 		return (ImageResizeService) services.get(ImageResizeService.class);
 	}
-
+	
 	public static ResourceService getResourceService() {
 		return (ResourceService) services.get(ResourceService.class);
+	}
+
+	public static ScreenTypeService getScreenTypeService() {
+		return (ScreenTypeService) services.get(ScreenTypeService.class);
 	}
 }
