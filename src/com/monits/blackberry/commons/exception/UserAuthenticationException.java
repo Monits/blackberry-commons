9a166b28998d0370643342ef0b21f47179e07122
Rodrigo Pereyra <rpereyra@monits.com>
2012-04-19 14:27:42 -0300

    Add UserAuthenticationException

---
+++ src/com/monits/blackberry/commons/exception/UserAuthenticationException.java
@@ -0,0 +1,25 @@
+/**
+ * AuthenticationExeption.java
+ * 
+ * @copyright 2011 Monits
+ * @license   Copyright (C) 2011. All rights reserved
+ * @version   Release: 1.0.0
+ * @link      http://www.monits.com/
+ * @since     1.0.0
+ */
+package com.monits.blackberry.commons.exception;
+
+
+/**
+ * AuthenticationExeption.java
+ * 
+ * @author    lbritez
+ * @copyright 2011 Monits
+ * @license   Copyright (C) 2011. All rights reserved
+ * @version   Release: 1.0.0
+ * @link      http://www.monits.com/
+ * @since     1.0.0
+ */
+public class UserAuthenticationException extends Exception {
+
+}
