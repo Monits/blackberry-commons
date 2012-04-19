89f44452b3a949ee06a232501ae805e731d97482
Rodrigo Pereyra <rpereyra@monits.com>
2012-04-19 15:42:13 -0300

    Add ResourcesService to the ServiceLocator

--- src/com/monits/blackberry/commons/services/ServiceLocator.java
+++ src/com/monits/blackberry/commons/services/ServiceLocator.java
@@ -3,6 +3,7 @@ package com.monits.blackberry.commons.services;
 import java.util.Hashtable;
 
 import com.monits.blackberry.commons.services.implementations.ImageResizeServiceImpl;
+import com.monits.blackberry.commons.services.implementations.ResourceServiceImpl;
 
 public class ServiceLocator {
 	private static Hashtable services;
@@ -10,9 +11,14 @@ public class ServiceLocator {
 	static {
 		services = new Hashtable();
 		services.put(ImageResizeService.class, new ImageResizeServiceImpl());
+		services.put(ResourceService.class, new ResourceServiceImpl());
 	}
 
 	public static ImageResizeService getImageResizeService() {
 		return (ImageResizeService) services.get(ImageResizeService.class);
+	}
+
+	public static ResourceService getResourceService() {
+		return (ResourceService) services.get(ResourceService.class);
 	}
 }
